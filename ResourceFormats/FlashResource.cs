6b17d240465ef6790d4454c1c0c432daf1a7c3a0
Jadcod <y.u.s.i.k@mail.ru>
2020-06-03 22:32:43 +0800

    Update FlashResource.cs

--- ResourceFormats/FlashResource.cs
+++ ResourceFormats/FlashResource.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 using FileFormats;
 using Gibbed.IO;
@@ -7,15 +7,13 @@ namespace ResourceFormats
 {
     public class FlashResource : IResourceType
     {
-        public string Name; // Path
-        //public string Short;
+        public string Path;
+        public string Short;
         public byte[] Data;
 
         public void Serialize(ushort version, Stream output, Endian endian)
         {
-            string Short = Path.GetFileNameWithoutExtension(Name);
-            output.WriteStringU16(Name, endian);
-            //output.WriteValueU64(_Hash, endian);
+            output.WriteStringU16(Path, endian);
             output.WriteValueU64(FileFormats.Hashing.FNV64.Hash(Short), endian);
             output.WriteStringU16(Short, endian);
             output.WriteValueU32((uint)Data.Length, endian);
@@ -24,28 +22,11 @@ namespace ResourceFormats
 
         public void Deserialize(ushort version, Stream input, Endian endian)
         {
-            Name = input.ReadStringU16(endian);
+            Path = input.ReadStringU16(endian);
             input.ReadValueU64(endian); // Hash
-            //Short = input.ReadStringU16(endian);
-            input.ReadStringU16(endian);
+            Short = input.ReadStringU16(endian);
             uint size = input.ReadValueU32(endian);
             Data = input.ReadBytes(size);
-
-#if _DEBUG
-            // weapon_icons_inworld_i71.dds
-            // vehicle_delivery_i2f.dds
-            /*string buf = null;
-            for (int i = 1; i < 256; i++)
-            {
-                buf += string.Format("hud_vehicle_i{0:x}.dds\r\n", i);
-            }*/
-
-            string sh = Path.GetFileNameWithoutExtension(Name);
-            if (sh != Short)
-            {
-                System.Diagnostics.Debugger.Break();
-            }
-#endif
         }
 
     }
